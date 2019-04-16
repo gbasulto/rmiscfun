f71734bfae71852686e39f806f0cc1477483603d
Basulto-Elias <basulto@iastate.edu>
2019-04-16 13:31:08 -0500

    Add another test.

--- tests/testthat/test-glance_data.R
+++ tests/testthat/test-glance_data.R
@@ -2,5 +2,16 @@ context("glance data function")
 
 
 test_that("nas expected", {
-  expect_true(all(is.na(as.vector(glance_data(iris)[5, 4:6]))))
+  all_nas <- as.vector(glance_data(iris)[5, 4:6])
+  expect_true(all(is.na(all_nas)))
 })
+
+test_that("check types",{
+  output <- glance_data(iris)$type
+  comparison <-
+    output == c("numerical", "numerical", "numerical",
+                "numerical", "factor")
+  all_good <- all(comparison)
+expect_true(all_good)
+})
+
